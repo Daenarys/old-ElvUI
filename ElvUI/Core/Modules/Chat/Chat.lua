2b1289ee029f491623e6ce59dcf89c00319206d0
Daenarys <androidwoz@gmail.com>
2022-01-26 13:25:58 +0100

    change voice chat icon pos

--- ElvUI/Core/Modules/Chat/Chat.lua
+++ ElvUI/Core/Modules/Chat/Chat.lua
@@ -3016,7 +3016,7 @@ function CH:HandleChatVoiceIcons()
 			button:ClearAllPoints()
 
 			if index == 1 then
-				button:Point('RIGHT', _G.GeneralDockManager, 'RIGHT', 2, 0)
+				button:Point('RIGHT', _G.GeneralDockManager, 'RIGHT', 7, 0)
 			else
 				button:Point('RIGHT', channelButtons[index-1], 'LEFT')
 			end
